1647ff0f00e0fb346084e4584c5168a2b83a8e1e
Szili Péter <szilipeti.x0@gmail.com>
2018-05-04 04:06:22 -0700

    Renamed FAILURE_RATE

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -7,7 +7,7 @@ import java.util.Random;
 */
 public class TorpedoStore {
 
-  private double FAILURE_RATE = 0.0;
+  private double failureRate = 0.0;
 
   private int torpedoCount = 0;
   private Random generator = new Random();
@@ -19,9 +19,9 @@ public class TorpedoStore {
     String failureEnv = System.getenv("IVT_RATE");
     if (failureEnv != null){
       try {
-        FAILURE_RATE = Double.parseDouble(failureEnv);
+        failureRate = Double.parseDouble(failureEnv);
       } catch (NumberFormatException nfe) {
-        FAILURE_RATE = 0.0;
+        failureRate = 0.0;
       }
     }
   }
@@ -36,7 +36,7 @@ public class TorpedoStore {
     // simulate random overheating of the launcher bay which prevents firing
     double r = generator.nextDouble();
 
-    if (r >= FAILURE_RATE) {
+    if (r >= failureRate) {
       // successful firing
       this.torpedoCount -= numberOfTorpedos;
       success = true;
